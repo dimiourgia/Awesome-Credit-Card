db20c760106c22b428eb8ab2711a0f11e3364279
ajay <ajayaxes318@gmail.com>
2022-11-22 21:04:19 +0530

    second commit

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const App = ()=>{
     return(
-        <h1>custom configured react app</h1>
+        <>
+            <h1>custom configured react app</h1>
+            <error>something dosent seems right</error>
+        </>
     )
 }
 
