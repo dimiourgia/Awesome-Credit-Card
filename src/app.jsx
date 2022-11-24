eac0683201b4ed39de1f3d4630d7c5e8379dced3
unknown <ajayaxes318@gmail.com>
2022-11-24 05:48:23 -0800

    compatiblity check

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 
 const App = ()=>{
-    return(
-        <>
-            <h1>custom configured react app</h1>
-            <error>something dosent seems right</error>
-        </>
-    )
 }
 
 export default App;
